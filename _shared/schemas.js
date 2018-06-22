144c91abcad07499a6a3738de1866113705bfc3d
Cory Robinson <crobinson42@users.noreply.github.com>
2018-06-22 09:07:07 -0700

    Update schemas.js
    fixes https://github.com/js-data/js-data/issues/401

--- _shared/schemas.js
+++ _shared/schemas.js
@@ -1,33 +1,36 @@
-'use strict'
+import { Schema } from 'js-data';
 
-module.exports = function (Schema) {
-  return {
-    user: new Schema({
-      properties: {
-        id: { type: 'string' },
-        name: { type: 'string' }
-      }
-    }),
-
-    post: new Schema({
-      properties: {
-        id: { type: 'string' },
-        // Only the DataStore component cares about the "indexed" attribute
-        user_id: { type: 'string', indexed: true },
-        title: { type: 'string' },
-        content: { type: 'string' }
-      }
-    }),
-
-    comment: new Schema({
-      properties: {
-        id: { type: 'string' },
-        // Only the DataStore component cares about the "indexed" attribute
-        post_id: { type: 'string', indexed: true },
-        // Only the DataStore component cares about the "indexed" attribute
-        user_id: { type: 'string', indexed: true },
-        content: { type: 'string' }
-      }
-    })
+export const user = new Schema({
+  $schema: 'http://json-schema.org/draft-04/schema#', // optional
+  title: 'User', // optional
+  description: 'Schema for User records', // optional
+  type: 'object',
+  properties: {
+    id: { type: 'number' },
+    name: { type: 'string' }
   }
-}
+});
+
+export const post = new Schema({
+  type: 'object',
+  properties: {
+    id: { type: 'number' },
+    // Only the DataStore and SimpleStore components care about the "indexed" attribute
+    user_id: { type: 'number', indexed: true },
+    title: { type: 'string' },
+    content: { type: 'string' },
+    date_published: { type: ['string', 'null'] }
+  }
+});
+
+export const comment = new Schema({
+  type: 'object',
+  properties: {
+    id: { type: 'number' },
+    // Only the DataStore and SimpleStore components care about the "indexed" attribute
+    post_id: { type: 'number', indexed: true },
+    // Only the DataStore and SimpleStore components care about the "indexed" attribute
+    user_id: { type: 'number', indexed: true },
+    content: { type: 'string' }
+  }
+});
